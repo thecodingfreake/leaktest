922884a13b8203f3c15ce49c0ce29c0ee8f8b798
thecodingfreake <114638726+thecodingfreake@users.noreply.github.com>
2024-11-02 20:15:57 +0530

    Update Form.jsx

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -12,7 +12,7 @@ const Form = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        fetch('http://localhost:5000/data/')
+        fetch('https://leaktestb.onrender.com/data/')
             .then(response => response.json())
             .then(data => setDropdownData(data))
             .catch(error => console.error('Error fetching dropdown data:', error));
