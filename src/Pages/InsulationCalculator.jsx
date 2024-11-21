7945ab8494d5007c2e4ecdc3fd5a9df4325f231d
thecodingfreake <114638726+thecodingfreake@users.noreply.github.com>
2024-11-21 20:46:04 +0530

    Update InsulationCalculator.jsx

--- src/Pages/InsulationCalculator.jsx
+++ src/Pages/InsulationCalculator.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import image from "../assests/logo.png"
+import './insu.css'
 
 const InsulationCalculator = () => {
   const [selectedInsulation, setSelectedInsulation] = useState('');
