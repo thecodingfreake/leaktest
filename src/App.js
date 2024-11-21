7cd8f0e90d8b31bdb4f51ce19377a11500526d52
thecodingfreake <114638726+thecodingfreake@users.noreply.github.com>
2024-11-21 20:15:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 
 import Form from './Pages/Form';
 import Download from './Pages/Download';
-
+import InsulationCalculator from './Pages/InsulationCalculator'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' Component={Form}></Route>
         <Route path='/download' Component={Download}></Route>
+        <Route path='/insulaion' Component={InsulationCalculator}></Route>
+
       </Routes>
     </BrowserRouter>
   );
